49df7151b63c5d2ecdda6e17ca7e0d5a6ec69270
Mire Haskim <mirehascim@gmail.com>
2025-03-09 12:56:35 -0500

    initial commit

---
+++ src/main/java/com/mireHaskim/MediaVault/MediaVaultApplication.java
@@ -0,0 +1,13 @@
+package com.mireHaskim.MediaVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaVaultApplication.class, args);
+	}
+
+}
